df7b4ae0ad42c2e02d6dc2c8815b865bc716a52e
OmarTarekFahmy <omarico593@gmail.com>
2025-11-12 11:28:53 +0200

    Created base core and parser classes

---
+++ src/core/BranchHandler.java
@@ -0,0 +1,9 @@
+package core;
+
+public class BranchHandler {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
---
+++ src/parser/ProgramLoader.java
@@ -0,0 +1,6 @@
+package src.parser;
+
+public class ProgramLoader {
+
+    
+}
